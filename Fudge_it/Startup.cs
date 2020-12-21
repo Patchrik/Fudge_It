866419eb0325e68cfeb840a625b96ecad06fb035
Patchrik <pklly92@gmail.com>
2020-12-21 09:57:38 -0600

    Added an Index view to make sure that the DB was connected correctly.

--- Fudge_it/Startup.cs
+++ Fudge_it/Startup.cs
@@ -1,3 +1,4 @@
+using Fudge_it.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace Fudge_it
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<IUserRepository, UserRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
